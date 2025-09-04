2f2db14e38a48595af691933f49428fc5aabd302
Danielpereiradasilva <daniel.pereira.silva30@escola.pr.gov.br>
2025-09-04 10:54:17 -0300

    ok

--- script.js
+++ script.js
@@ -2,5 +2,18 @@ const avanca = document.querySelectorAll('.btn-proximo');
 const reiniciarBtn = document.getElementById('bth-reiniciar');
 
 avance.forEach(button =>
-button.addEventListener('click')
+       button.addEventListener('click',function() {
+       const atual = document.querySelector('.ativo');
+       const proximoPasso = 'passo-'+this.getAttribute('data-proximo');
+
+       atual.classList.remove('ativo');
+       atual.proximoElemento= document.getElementsById(proximoPasso);
+
+       if (proximoElemento){
+           proximoElemento.classList.add('ativo');
+       } else {
+           console.error(`Elemento com ID "${proximoPasso}" não encontrado.`)
+       }
+    })
+
 )
